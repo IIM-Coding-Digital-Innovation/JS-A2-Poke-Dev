d6fa4fa16a1b6a34dffa66a1b9b9a8dcc362f9f3
Lucas Charoing <charoing.lucas@gmail.com>
2023-01-13 11:46:23 +0100

    Add evolutions and ids

--- pokedex.js
+++ pokedex.js
@@ -17,210 +17,283 @@ let overview = document.querySelector('#overview-card');
 let evolutions;
 let speciesUrl;
 
-document.addEventListener('keydown', function(event) {
-    keyboardLog += event.key;
-    if (keyboardLog.endsWith('shiny')) {
-        cards.forEach(card => {
-            card.classList.add('shiny');
-            isShiny = true;
-        })
-        keyboardLog = '';
-    }
+document.addEventListener('keydown', function (event) {
+  keyboardLog += event.key;
+  if (keyboardLog.endsWith('shiny')) {
+    document.querySelector('body').classList.toggle('shiny');
+    keyboardLog = '';
+  }
+  if (document.activeElement === searchInput && event.key === 'Enter') {
+    displayPokemonInfos(searchInput.value);
+  }
 });
 
 exit.addEventListener('click', () => {
-    overviewParent.style.display = null;
-})
+  overviewParent.style.display = null;
+});
 
 searchButton.addEventListener('click', () => {
-    displayPokemonInfos(searchInput.value);
-})
+  displayPokemonInfos(searchInput.value);
+});
 
 loadPage(nextUrl);
 
 previousButton.addEventListener('click', () => {
-    if (previousUrl != '') {
-        loadPage(previousUrl);
-    }
-})
+  if (previousUrl != '') {
+    loadPage(previousUrl);
+  }
+  overviewParent.style.display = null;
+});
 
 nextButton.addEventListener('click', () => {
-    if (nextUrl != '') {
-        loadPage(nextUrl);
-    }
-})
+  if (nextUrl != '') {
+    loadPage(nextUrl);
+  }
+  overviewParent.style.display = null;
+});
 
 function loadPage(url) {
-    fetch(url)
-    .then(function(response) {
-        return response.json();
+  fetch(url)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-        previousUrl = !(data.previous === null) ? data.previous:'';
-        nextUrl = !(data.next === null) ? data.next:'';
+    .then(function (data) {
+      previousUrl = !(data.previous === null) ? data.previous : '';
+      nextUrl = !(data.next === null) ? data.next : '';
 
-        pokedexDictionary.innerHTML = '';
+      pokedexDictionary.innerHTML = '';
 
-        let pokemonPromises = data.results.map(pokemon => {
-            return fetch(pokemon.url).then(response => response.json());
+      let pokemonPromises = data.results.map((pokemon) => {
+        return fetch(pokemon.url).then((response) => response.json());
+      });
+      console.log(data.results);
+      Promise.all(pokemonPromises)
+        .then((pokemonData) => {
+          pokemonData.forEach((data, index) => {
+            let card = document.createElement('a');
+            card.classList.add('pokemon-card');
+            let noise = document.createElement('img');
+            noise.classList.add('noise');
+            noise.src = './img/noise.jpg';
+            let img = document.createElement('img');
+            img.classList.add('regular');
+            img.src = data.sprites.front_default;
+            let imgShiny = document.createElement('img');
+            imgShiny.classList.add('shiny');
+            imgShiny.src = data.sprites.front_shiny;
+            let infos = document.createElement('div');
+            let name = document.createElement('span');
+            name.innerHTML = data.name;
+            let id = document.createElement('span');
+            id.innerHTML = '#' + data.id;
+            let stats = document.createElement('ul');
+            let hp = document.createElement('li');
+            let hpData = document.createElement('text');
+            hpData.innerHTML =
+              '<span>HP...............................................................................</span><span></span><span>' +
+              data.stats[0].base_stat +
+              '</span>';
+            hp.appendChild(hpData);
+            let attack = document.createElement('li');
+            let attackData = document.createElement('text');
+            attackData.innerHTML =
+              '<span>Attack...............................................................................</span><span>' +
+              data.stats[1].base_stat +
+              '</span>';
+            attack.appendChild(attackData);
+            let defense = document.createElement('li');
+            let defenseData = document.createElement('text');
+            defenseData.innerHTML =
+              '<span>Defense...............................................................................</span><span>' +
+              data.stats[2].base_stat +
+              '</span>';
+            defense.appendChild(defenseData);
+
+            card.addEventListener('click', () => {
+              displayPokemonInfos(data.name);
+            });
+
+            stats.appendChild(hp);
+            stats.appendChild(attack);
+            stats.appendChild(defense);
+
+            name.appendChild(id);
+
+            infos.appendChild(name);
+            infos.appendChild(stats);
+
+            card.appendChild(noise);
+            card.appendChild(img);
+            card.appendChild(imgShiny);
+            card.appendChild(infos);
+
+            pokedexDictionary.appendChild(card);
+          });
         })
-        console.log(data.results);
-        Promise.all(pokemonPromises).then(pokemonData => {
-            pokemonData.forEach((data, index) => {
-                let card = document.createElement('a');
-                card.classList.add('pokemon-card');
-                if (isShiny) {
-                    card.classList.add('shiny');
-                }
-                let noise = document.createElement('img');
-                noise.classList.add('noise');
-                noise.src = './img/noise.jpg';
-                let img = document.createElement('img');
-                img.classList.add('regular');
-                img.src = data.sprites.front_default;
-                let imgShiny = document.createElement('img');
-                imgShiny.classList.add('shiny');
-                imgShiny.src = data.sprites.front_shiny;
-                let infos = document.createElement('div');
-                let name = document.createElement('span');
-                name.innerHTML = data.name;
-                let stats = document.createElement('ul');
-                let hp = document.createElement('li');
-                let hpData = document.createElement('text');
-                hpData.innerHTML = '<span>HP...............................................................................</span><span></span><span>' + data.stats[0].base_stat + '</span>';
-                hp.appendChild(hpData);
-                let attack = document.createElement('li');
-                let attackData = document.createElement('text');
-                attackData.innerHTML = '<span>Attack...............................................................................</span><span>' + data.stats[1].base_stat + '</span>';
-                attack.appendChild(attackData);
-                let defense = document.createElement('li');
-                let defenseData = document.createElement('text');
-                defenseData.innerHTML = '<span>Defense...............................................................................</span><span>' + data.stats[2].base_stat + '</span>';
-                defense.appendChild(defenseData);
-
-                card.addEventListener('click', () => {
-                    displayPokemonInfos(data.name);
-                })
-
-                stats.appendChild(hp);
-                stats.appendChild(attack);
-                stats.appendChild(defense);
-
-                infos.appendChild(name);
-                infos.appendChild(stats);
-
-                card.appendChild(noise);
-                card.appendChild(img);
-                card.appendChild(imgShiny);
-                card.appendChild(infos);
-
-                pokedexDictionary.appendChild(card);
-            })
-        }).then(() => {
-            cards = document.querySelectorAll('a');
-        })
+        .then(() => {
+          cards = document.querySelectorAll('a');
+        });
     })
-    .catch(function(error) {
-        console.log(error);
-    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 function displayPokemonInfos(pokemonName) {
-    overview.innerHTML = '';
-    fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
-    .then(function(response) {
-        return response.json();
+  overview.innerHTML = '';
+  fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-        console.log(data);
+    .then(function (data) {
+      console.log(data);
 
-        speciesUrl = data.species.url;
+      speciesUrl = data.species.url;
 
-        let primaryPokemon = document.createElement('div');
-        primaryPokemon.classList.add('primary-pokemon');
+      let primaryPokemon = document.createElement('div');
+      primaryPokemon.classList.add('primary-pokemon');
 
-        let img = document.createElement('img');
-        img.src = data.sprites.front_default;
+      let img = document.createElement('img');
+      img.classList.add('regular');
+      img.src = data.sprites.front_default;
 
-        let name = document.createElement('span');
-        name.innerHTML = data.name;
+      let imgShiny = document.createElement('img');
+      imgShiny.classList.add('shiny');
+      imgShiny.src = data.sprites.front_shiny;
 
-        let types = document.createElement('ul');
-        types.classList.add('types');
-        data.types.forEach(type => {
-            let typeLi = document.createElement('li');
-            typeLi.innerHTML = type.type.name;
-            types.appendChild(typeLi);
-        })
-        
-        let title = document.createElement('h3');
-        title.innerHTML = 'Evolutions';
+      let pokemonInfos = document.createElement('div');
+      pokemonInfos.classList.add('pokemon-infos');
 
-        evolutions = document.createElement('ul');
-        evolutions.classList.add('evolutions');
+      let name = document.createElement('span');
+      name.innerHTML = data.name;
 
-        primaryPokemon.appendChild(img);
-        primaryPokemon.appendChild(name);
-        primaryPokemon.appendChild(types);
-        overview.appendChild(primaryPokemon);
-        overview.appendChild(title);
-        overview.appendChild(evolutions);
+      let id = document.createElement('span');
+      id.innerHTML = '#' + data.id;
+      name.appendChild(id);
+
+      let stats = document.createElement('ul');
+      stats.classList.add('stats');
+      let hp = document.createElement('li');
+      let hpData = document.createElement('text');
+      hpData.innerHTML =
+        '<span>HP: </span><span></span><span>' +
+        data.stats[0].base_stat +
+        '</span>';
+      hp.appendChild(hpData);
+      let attack = document.createElement('li');
+      let attackData = document.createElement('text');
+      attackData.innerHTML =
+        '<span>Attack: </span><span>' + data.stats[1].base_stat + '</span>';
+      attack.appendChild(attackData);
+      let defense = document.createElement('li');
+      let defenseData = document.createElement('text');
+      defenseData.innerHTML =
+        '<span>Defense: </span><span>' + data.stats[2].base_stat + '</span>';
+      defense.appendChild(defenseData);
+
+      stats.appendChild(hp);
+      stats.appendChild(attack);
+      stats.appendChild(defense);
+
+      let types = document.createElement('ul');
+      types.classList.add('types');
+      data.types.forEach((type) => {
+        let typeLi = document.createElement('li');
+        typeLi.classList.add(type.type.name);
+        typeLi.innerHTML = type.type.name;
+        types.appendChild(typeLi);
+      });
+
+      let title = document.createElement('h3');
+      title.innerHTML = 'Evolutions';
+
+      evolutions = document.createElement('ul');
+      evolutions.classList.add('evolutions');
+
+      primaryPokemon.appendChild(img);
+      primaryPokemon.appendChild(imgShiny);
+      pokemonInfos.appendChild(name);
+      pokemonInfos.appendChild(stats);
+      pokemonInfos.appendChild(types);
+      primaryPokemon.appendChild(pokemonInfos);
+      overview.appendChild(primaryPokemon);
+      overview.appendChild(title);
+      overview.appendChild(evolutions);
     })
     .then(() => {
-        fetch(speciesUrl)
-        .then(function(response) {
-            return response.json();
+      fetch(speciesUrl)
+        .then(function (response) {
+          return response.json();
         })
-        .then(function(data) {
-            console.log(data);
-            fetch(data.evolution_chain.url)
-            .then(function(response) {
-                return response.json();
+        .then(function (data) {
+          console.log(data);
+          fetch(data.evolution_chain.url)
+            .then(function (response) {
+              return response.json();
             })
-            .then(function(data) {
-                console.log(data.chain);
-                displayEvolutions(evolutions, data.chain)
-            })
-        })
+            .then(function (data) {
+              console.log(data.chain);
+              displayEvolutions(evolutions, data.chain);
+            });
+        });
     })
+    .catch(function (error) {
+      let errorMessage = document.createElement('text');
+      errorMessage.innerHTML = 'No Pokemon found';
+      overview.appendChild(errorMessage);
+      overviewParent.style.display = 'flex';
+    });
 }
 
 function displayEvolutions(parent, chain) {
-    let evolution = document.createElement('li');
+  let evolution = document.createElement('li');
 
-    fetch('https://pokeapi.co/api/v2/pokemon/' + chain.species.name)
-    .then(function(response) {
-        return response.json();
+  fetch('https://pokeapi.co/api/v2/pokemon/' + chain.species.name)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-        console.log(data);
-        let img = document.createElement('img');
-        img.src = data.sprites.front_default;
-        let name = document.createElement('span');
-        name.innerHTML = data.name;
-        let types = document.createElement('ul');
-        types.classList.add('types');
-        data.types.forEach(type => {
-            let typeLi = document.createElement('li');
-            typeLi.innerHTML = type.type.name;
-            types.appendChild(typeLi);
-        })
+    .then(function (data) {
+      console.log(data);
+      let img = document.createElement('img');
+      img.classList.add('regular');
+      img.src = data.sprites.front_default;
+      let imgShiny = document.createElement('img');
+      imgShiny.classList.add('shiny');
+      imgShiny.src = data.sprites.front_shiny;
+      let name = document.createElement('span');
+      name.innerHTML = data.name;
+      let id = document.createElement('span');
+      id.innerHTML = '#' + data.id;
+      name.appendChild(id);
+      let types = document.createElement('ul');
+      types.classList.add('types');
+      data.types.forEach((type) => {
+        let typeLi = document.createElement('li');
+        typeLi.classList.add(type.type.name);
+        typeLi.innerHTML = type.type.name;
+        types.appendChild(typeLi);
+      });
 
-        evolution.addEventListener('click', () => {
-            displayPokemonInfos(data.name);
-        })
-    
-        evolution.appendChild(img);
-        evolution.appendChild(name);
-        evolution.appendChild(types);
-        evolutions.appendChild(evolution);
+      evolution.addEventListener('click', () => {
+        displayPokemonInfos(data.name);
+      });
 
-        if (chain.evolves_to.length > 0) {
-            chain.evolves_to.forEach(newChain => {
-                displayEvolutions(parent, newChain);
-            })
-        } else {
-            overviewParent.style.display = 'flex';
-        }
-    })
+      evolution.appendChild(img);
+      evolution.appendChild(imgShiny);
+      evolution.appendChild(name);
+      evolution.appendChild(types);
+      evolutions.appendChild(evolution);
+
+      if (chain.evolves_to.length > 0) {
+        let separator = document.createElement('li');
+        separator.classList.add('separator');
+        separator.innerHTML = '>';
+        evolutions.appendChild(separator);
+        chain.evolves_to.forEach((newChain) => {
+          displayEvolutions(parent, newChain);
+        });
+      } else {
+        overviewParent.style.display = 'flex';
+      }
+    });
 }
