6e2efa79ff456712c572b2be4fa2e9d96828e39e
Fanny Gautier <fannygautier92@gmail.com>
2023-01-13 16:07:40 +0100

    Add damages for the attack

--- pokemonBattle/arcade.js
+++ pokemonBattle/arcade.js
@@ -9,13 +9,24 @@ let statsDiv
 let theMove
 let theMoves
 
+let firstPokemon
+let secondPokemon
+let currentAttack =[]
+let usedAttack
+
+
+let pokemonDead = false
+
 /*
 ======================== THE GAME  ====================================
 
 */
 
-let displayText=document.querySelector(".the-text")
+/*let displayText=document.querySelector(".the-text")*/
 
+function useMove(move){
+
+}
 
 
 
@@ -28,12 +39,78 @@ function chooseMove(){
     attacksDiv.addEventListener("click", function(event) {
         if (event.target.classList.contains('move-slot')) {
             console.warn(event.target)
+            console.log(currentAttack)
+            let theAttack = event.target.querySelector(".move-name")
+            console.log(theAttack)
+            let thePP = event.target.querySelector(".pp-move")
+            currentAttack.forEach(e=>{
+                console.log(e["name"])
+                console.log(theAttack.innerHTML)
+                if(e["name"]===theAttack.innerHTML){
+                    usedAttack=e
+                    damages(pickPokemon,secondPokemon,usedAttack)
+                }
+            })
         }
     });
     
 
 }
+/*
+======================== FUNCTION ennemmyDamages ====================================
 
+*/
+function ennemyAttacks(yourPokemon,ennemy){
+    console.log(ennemy)
+    let randomMoves = Math.floor(Math.random() * 4)
+    let ennemyattack=ennemy["moves"][randomMoves]
+    console.log(ennemyattack)
+    let query = ennemyattack["move"]["url"]
+
+    fetch(query)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        return response
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+    let infoText = document.querySelector(".the-text")
+    let degats = response["power"];
+    infoText.innerHTML = ennemy["name"] + " lance " + attack["name"] +", " +yourPokemon["name"] + " lost " + degats + " HP";
+    let newhp = yourPokemon["stats"][0]["base_stat"] - degats
+    
+
+    if(newhp<0){
+        pokemonDead = true
+    }else{
+        ennemyAttacks(yourPokemon,ennemy)
+    }é
+}
+
+
+/*
+======================== FUNCTION DAMAGES ====================================
+
+*/
+
+function damages(yourPokemon,ennemy,attack){
+    let infoText = document.querySelector(".the-text")
+    let degats = attack["power"];
+    infoText.innerHTML = yourPokemon["name"] + " lance " + attack["name"] +", " +ennemy["name"] + " lost " + degats + " HP";
+    let newhp = ennemy["stats"][0]["base_stat"] - degats
+    ennemyHp.innerHTML="HP : "+newhp
+
+    if(newhp<0){
+        pokemonDead = true
+    }else{
+        ennemyAttacks(yourPokemon,ennemy)
+    }
+    return degats
+
+}
 
 /*
 ======================== FUNCTION SEARCH ATTACKS INFO ====================================
@@ -48,13 +125,44 @@ function searchForReal(query){
         return response.json();
     })
     .then(function(response){
+        console.log(response)
         let moveSlot = document.createElement("div")
         moveSlot.classList.add("move-slot")
-        moveSlot.innerHTML+=response["name"] + " "
-        moveSlot.innerHTML+=response["flavor_text_entries"][8]["flavor_text"]+ " "
-       
-        attacksSlot.appendChild(moveSlot)
+        let textMove = document.createElement("H3")
+        textMove.classList.add("move-name")
+        let nameMove = document.createElement("p")
+        let dataMove = document.createElement("ul")
+        let accuracy =document.createElement("li")
+        let damage =document.createElement("li")
+        let pp=document.createElement("li")
+        pp.classList.add("pp-move")
+
+        pp.innerHTML= "PP : " + response["pp"]
+
+        if(response["accuracy"]===null){
+            accuracy.innerHTML= "Accuracy : x "
+        }else{
+            accuracy.innerHTML= "Accuracy : "+response["accuracy"]
+        }
+        if(response["power"]===null){
+            damage.innerHTML= "Power : x "
+        }else{
+            damage.innerHTML="Power : "+ response["power"]
+        }
         
+    
+
+        dataMove.appendChild(accuracy);
+        dataMove.appendChild(damage);
+        dataMove.appendChild(pp)
+
+        textMove.innerHTML=response["name"]
+        nameMove.innerHTML=response["flavor_text_entries"][8]["flavor_text"]+ " "
+        moveSlot.appendChild(textMove)
+        moveSlot.appendChild(nameMove)
+        moveSlot.appendChild(dataMove)
+        attacksSlot.appendChild(moveSlot)
+        return currentAttack.push(response)
     })
     .catch(function(error){
         console.log(error);
@@ -73,6 +181,7 @@ function createStatsCard(stats){
     statsDiv=document.createElement("div")
     statsDiv.classList.add("stats-div")
     stats.forEach(e=>{
+        if(e["stat"]["name"]=""){}
         let stats=document.createElement("div")
         stats.classList.add('little-stats')
         stats.innerHTML+=e["stat"]["name"]+" :"
@@ -143,6 +252,7 @@ function search(query){
         ennemyName.innerHTML=response["name"];
         ennemyImg.src=response["sprites"]["front_default"]
         ennemyHp.innerHTML+=response["stats"][0]["base_stat"]
+        return secondPokemon = response
     })
     .catch(function(error){
         console.log(error);
@@ -191,15 +301,19 @@ let gameLoopPromise=new Promise((resolve,reject)=>{
 
                                 for(let i=0 ; i<4 ; i++){
                                     console.log(pickPokemonMoves.length)
-                                    let randomMoves = Math.floor(Math.random() * 99)
-                                    pickPokemonMoves.push(pickPokemon["moves"][randomMoves])
-                                    console.log(pickPokemonMoves)
+                                    let randomMoves = Math.floor(Math.random() * 50)
+                                 
+                                        pickPokemonMoves.push(pickPokemon["moves"][randomMoves])
+                                        console.log(pickPokemonMoves)
+                                    
+                                   
                                 }
                         }
                         if(pickPokemonMoves.length===4){
                                 displayMoves(pickPokemonMoves);
                                 
                                 resolve();
+                                return firstPokemon = e
                                 
                         }
                 }
